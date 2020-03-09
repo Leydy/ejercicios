66ea5f40c251a3d0952bae41770c7bb2c493bb63
Leydy <leydymayumy@gmail.com>
2020-03-09 17:01:50 -0500

    Falta el 8

--- ejercicio9.js
+++ ejercicio9.js
@@ -1,1 +1,15 @@
 var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function Suma() {
+    var par = 0;
+    var impar = 0;
+    for(const value of a) {
+        if (value % 2 === 0){
+            par = par + value;
+        } else {
+            impar = impar + value;
+        }
+    }
+    console.log("Suma de pares: "+par);
+    console.log("Suma de impares: "+impar);
+}
+Suma();
